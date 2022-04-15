f13951a1f77eeb95312e3d38da8c78cf34b908a2
Ghofar <ghofarsuprapto@gmail.com>
2022-04-15 15:19:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
 #include "fibonacci.h"
 
 int main() {
     fibonacci f;
-    std::cout << "Iteration method: "; f.kalkulasi_iterasi(6); std::cout << std::endl;
-    std::cout << "Recursive method: "; f.kalkulasi_rekursif(6); std::cout << std::endl;
+    std::cout << "Masukan input nya: ";
+    int input; std::cin >> input;
+
+    std::cout << "Iteration method: "; f.kalkulasi_iterasi(input); std::cout << std::endl;
+    std::cout << "Recursive method: "; f.kalkulasi_rekursif(input); std::cout << std::endl;
 }
